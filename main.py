24b8e784fb65d2d0496a6c5fe6a8f65ebd4d7a63
Amirgolbini <amirgol127@gmail.com>
2024-05-08 17:04:34 +0430

    adameh
    code samaneh

--- main.py
+++ main.py
@@ -7,6 +7,22 @@ class StudentManagementApp(tk.Tk):
 
         self.title("Student Management System")
         self.geometry("250x400")
+        self.creat_widget()
+        self.database = Database()
+
+    def creat_widget(self):
+        lbl_id = tk.Label(self, text='Meli code:')
+        lbl_id.grid(row=0, column=0, padx=10, pady=10)
+
+        lbl_last_name = tk.Label(self, text='Firts name:')
+        lbl_last_name.grid(row=1, column=0, padx=10, pady=10)
+
+        lbl_age = tk.Label(self, text='AGE:')
+        lbl_age.grid(row=2, column=0, padx=10, pady=10)
+
+        lbl_email = tk.Label(self, text='Email;')
+        lbl_email.grid(row=4, column=0, padx=10, pady=10)
+
 
 
 if __name__ == '__main__':
