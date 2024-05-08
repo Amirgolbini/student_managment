5b36097fb0fbbbe2443dda966aba8042a24e4c62
Amirgolbini <amirgol127@gmail.com>
2024-05-08 17:40:29 +0430

    entry
    added entry

--- main.py
+++ main.py
@@ -8,21 +8,38 @@ class StudentManagementApp(tk.Tk):
         self.title("Student Management System")
         self.geometry("250x400")
         self.creat_widget()
-        self.database = Database()
 
+    # labale field
     def creat_widget(self):
         lbl_id = tk.Label(self, text='Meli code:')
         lbl_id.grid(row=0, column=0, padx=10, pady=10)
 
-        lbl_last_name = tk.Label(self, text='Firts name:')
-        lbl_last_name.grid(row=1, column=0, padx=10, pady=10)
+        lbl_first_name = tk.Label(self, text='Firts name:')
+        lbl_first_name.grid(row=1, column=0, padx=10, pady=10)
+
+        lbl_last_name = tk.Label(self, text='last name:')
+        lbl_last_name.grid(row=2, column=0, padx=10, pady=10)
 
         lbl_age = tk.Label(self, text='AGE:')
-        lbl_age.grid(row=2, column=0, padx=10, pady=10)
+        lbl_age.grid(row=3, column=0, padx=10, pady=10)
 
         lbl_email = tk.Label(self, text='Email;')
         lbl_email.grid(row=4, column=0, padx=10, pady=10)
 
+        self.entry_id = tk.Entry(self)
+        self.entry_id.grid(row=0, column=1, padx=10, pady=10)
+
+        self.entry_first_name = tk.Entry(self)
+        self.entry_first_name.grid(row=1, column=1, padx=10, pady=10)
+
+        self.entry_last_name = tk.Entry(self)
+        self.entry_last_name.grid(row=2, column=1, padx=10, pady=10)
+
+        self.entry_age = tk.Entry(self)
+        self.entry_age.grid(row=3, column=1, padx=10, pady=10)
+
+        self.entry_email = tk.Entry(self)
+        self.entry_email.grid(row=4, column=1, padx=10, pady=10)
 
 
 if __name__ == '__main__':
