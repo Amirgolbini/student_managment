2defee43bfde81319f4a7cdc364c22bbdf19366a
Amirgolbini <amirgol127@gmail.com>
2024-05-29 21:53:23 +0330

    edit and delete
    edit and delete

--- db.py
+++ db.py
@@ -1,70 +1,56 @@
 import mysql.connector
 import tkinter.messagebox as messagebox
 
-
 class Database:
     def __init__(self):
         self.connection = mysql.connector.connect(
-<<<<<<< HEAD
             host='localhost',
             user='root',
-            password='0173276',
-            database='student_management'
-        )
-=======
-        host = 'localhost',
-        user = 'root',
-        password = 'amir1382',
-        database = 'student_management'
-)
->>>>>>> 5bed0d28272b8076ad726d2aa0abf3e56f15cd33
-
+            password='amir1382',
+            database='student_management')
         self.cursor = self.connection.cursor()
 
     def add_student(self, person):
         try:
-            query = "INSERT INTO student(meli, First_name, Last_name,age,email)VALUES (%s,%s,%s,%s,%s)"
-            person = (person.meli, person.first_name, person.last_name, person.age, person.email)
-            self.cursor.execute(query, person)
+            query = "INSERT INTO student(meli, first_name, last_name, age, email) VALUES (%s, %s, %s, %s, %s)"
+            person_data = (person.meli, person.first_name, person.last_name, person.age, person.email)
+            self.cursor.execute(query, person_data)
             self.connection.commit()
-            messagebox.showinfo("success", "Student added successfully")
+            messagebox.showinfo("Success", "Student added successfully")
+        except mysql.connector.Error as err:
+            messagebox.showerror("Error", f"Database error: {err}")
 
-        except Exception as e:
-            print(e)
-            messagebox.showwarning('Error', 'It can not to add to db.!')
-
-    def get_all_student(self):
+    def get_all_students(self):
         try:
             query = "SELECT * FROM student"
             self.cursor.execute(query)
             students = self.cursor.fetchall()
             return students
         except mysql.connector.Error as err:
-            messagebox.showerror("Error", f"Database error:{err}")
+            messagebox.showerror("Error", f"Database error: {err}")
             return None
 
-    def upadte_student(self, student_id, person):
+    def update_student(self, meli, person):
         try:
             query = """
-                UPDATE student SET meli =%s, first_name = %s , last_name = %s,age =%s,email = %s
+                UPDATE student SET meli = %s, first_name = %s, last_name = %s, age = %s, email = %s
                 WHERE meli = %s
-                
             """
-            person = (person.meli, person.first_name, person.last_name, person.age, person.email, student_id)
-            self.cursor.execute(query, person)
+            new_data = (person.meli, person.first_name, person.last_name, person.age, person.email, meli)
+            self.cursor.execute(query, new_data)
             self.connection.commit()
-            messagebox.showinfo("Success", "Student updated successfuolly!")
+            messagebox.showinfo("Success", "Student updated successfully")
         except mysql.connector.Error as err:
-            messagebox.showwarning("Error", f"Database error : {err}")
+            messagebox.showerror("Error", f"Database error: {err}")
 
-    def del_student(self, meli):
+    def delete_student(self, meli):
         try:
-            query = "DELETE FROM students WHERE meli = %s"
+            query = "DELETE FROM student WHERE meli = %s"
             self.cursor.execute(query, (meli,))
-            self.connection.comit()
+            self.connection.commit()
             if self.cursor.rowcount > 0:
-                messagebox.showinfo("Success", "Student deleted successfully!")
+                messagebox.showinfo("Success", "Student deleted successfully")
             else:
-                messagebox.showwarning("Error", "Student not found")
+                messagebox.showwarning("Warning", "Student not found")
         except mysql.connector.Error as err:
-            messagebox.showwarning("Error", f"Database error:{err}")
+            messagebox.showerror("Error", f"Database error: {err}")
--- main.py
+++ main.py
@@ -1,12 +1,9 @@
 import tkinter as tk
-
-import mysql.connector
-from PIL import Image, ImageTk
-from students import Person
 import tkinter.messagebox as messagebox
-from db import Database
+from PIL import Image, ImageTk
 from ttkbootstrap import Treeview
-
+from db import Database
+from students import Person
 
 class StudentManagementApp(tk.Tk):
     def __init__(self):
@@ -18,21 +15,20 @@ class StudentManagementApp(tk.Tk):
         self.create_widgets()
         self.database = Database()
 
-    # label field
     def create_widgets(self):
         lbl_id = tk.Label(self, text='Meli code:', bg="firebrick1", fg="white")
         lbl_id.grid(row=0, column=0, padx=10, pady=10)
 
-        lbl_first_name = tk.Label(self, text='Firts name:', bg="firebrick1", fg="white")
+        lbl_first_name = tk.Label(self, text='First name:', bg="firebrick1", fg="white")
         lbl_first_name.grid(row=1, column=0, padx=10, pady=10)
 
-        lbl_last_name = tk.Label(self, text='last name:', bg="firebrick1", fg="white")
+        lbl_last_name = tk.Label(self, text='Last name:', bg="firebrick1", fg="white")
         lbl_last_name.grid(row=2, column=0, padx=10, pady=10)
 
-        lbl_age = tk.Label(self, text='AGE:', bg="firebrick1", fg="white")
+        lbl_age = tk.Label(self, text='Age:', bg="firebrick1", fg="white")
         lbl_age.grid(row=3, column=0, padx=10, pady=10)
 
-        lbl_email = tk.Label(self, text='Email;', bg="firebrick1", fg="white")
+        lbl_email = tk.Label(self, text='Email:', bg="firebrick1", fg="white")
         lbl_email.grid(row=4, column=0, padx=10, pady=10)
 
         self.entry_id = tk.Entry(self, width=50)
@@ -50,28 +46,27 @@ class StudentManagementApp(tk.Tk):
         self.entry_email = tk.Entry(self, width=50)
         self.entry_email.grid(row=4, column=1, padx=10, pady=10)
 
-        btn_add = tk.Button(self, text='Add student:', command=self.add_student, bg="blue", fg="white")
+        btn_add = tk.Button(self, text='Add Student', command=self.add_student, bg="blue", fg="white")
         btn_add.grid(row=5, column=0, padx=10, pady=10)
 
-        btn_edit = tk.Button(self, text='Edit student:', command=self.edit_student, bg="blue", fg="white")
+        btn_edit = tk.Button(self, text='Edit Student', command=self.edit_student, bg="blue", fg="white")
         btn_edit.grid(row=5, column=1, padx=10, pady=10)
 
-        btn_view = tk.Button(self, text='View student', command=self.view_student, bg="blue", fg="white")
+        btn_view = tk.Button(self, text='View Students', command=self.view_students, bg="blue", fg="white")
         btn_view.grid(row=6, column=0, padx=10, pady=10)
 
-        btn_delete = tk.Button(self, text='delet Student', command=self.del_student, bg="blue", fg="white")
+        btn_delete = tk.Button(self, text='Delete Student', command=self.delete_student, bg="blue", fg="white")
         btn_delete.grid(row=6, column=1, padx=10, pady=10)
 
-        btn_clear = tk.Button(self, text='Clear student', command=self.clear_entries, bg="blue", fg="white")
+        btn_clear = tk.Button(self, text='Clear Entries', command=self.clear_entries, bg="blue", fg="white")
         btn_clear.grid(row=7, column=0, padx=10, pady=10)
 
         image = Image.open("F:/J/IMG_۲۰۲۰۰۷۱۷_۲۰۱۵۰۲.jpg")
-        image = image.resize((350, 200))  # Resize the image
+        image = image.resize((350, 200))
         photo = ImageTk.PhotoImage(image)
 
-        # Create a label to display the image
         image_label = tk.Label(self, image=photo)
-        image_label.image = photo  # Keep a reference to the image to prevent garbage collection
+        image_label.image = photo
         image_label.grid(row=0, column=2, rowspan=5, padx=10, pady=10)
 
     def add_student(self):
@@ -83,15 +78,10 @@ class StudentManagementApp(tk.Tk):
 
         if meli and first_name and last_name and email:
             person = Person(meli, first_name, last_name, age, email)
-<<<<<<< HEAD
-            # messagebox.showinfo("success", "Student added successfully!")
-=======
->>>>>>> 5bed0d28272b8076ad726d2aa0abf3e56f15cd33
             self.database.add_student(person)
             self.clear_entries()
-            # messagebox.showinfo("Success", "Student added successfully!")
         else:
-            messagebox.showwarning('Error', "Please fill in all the fields ")
+            messagebox.showwarning('Error', "Please fill in all the fields")
 
     def edit_student(self):
         if self.selected_student:
@@ -118,56 +108,39 @@ class StudentManagementApp(tk.Tk):
             self.entry_edit_last_name.grid(row=2, column=1, padx=10, pady=10)
             self.entry_edit_last_name.insert(tk.END, student_data[2])
 
-            lbl_last_age = tk.Label(edit_window, text="age :")
-            lbl_last_age.grid(row=3, column=0, padx=10, pady=10)
+            lbl_age = tk.Label(edit_window, text="Age:")
+            lbl_age.grid(row=3, column=0, padx=10, pady=10)
             self.entry_edit_age = tk.Entry(edit_window)
             self.entry_edit_age.grid(row=3, column=1, padx=10, pady=10)
             self.entry_edit_age.insert(tk.END, student_data[3])
 
-            lbl_email = tk.Label(edit_window, text="Email :")
+            lbl_email = tk.Label(edit_window, text="Email:")
             lbl_email.grid(row=4, column=0, padx=10, pady=10)
-            self.entry_email = tk.Entry(edit_window)
-            self.entry_email.grid(row=4, column=1, padx=10, pady=10)
-            self.entry_email.insert(tk.END, student_data[4])
+            self.entry_edit_email = tk.Entry(edit_window)
+            self.entry_edit_email.grid(row=4, column=1, padx=10, pady=10)
+            self.entry_edit_email.insert(tk.END, student_data[4])
 
-            btn_save = tk.Button(edit_window, text="Save Change",
-                                 command=lambda: self.database.upadte_student(self.selected_student[0],
-                                                                              Person(self.entry_edit_id.get(),
-                                                                                     self.entry_edit_first_name.get(),
-                                                                                     self.entry_edit_last_name.get(),
-                                                                                     self.entry_edit_age.get(),
-                                                                                     self.entry_email.get())))
+            btn_save = tk.Button(edit_window, text="Save Changes", command=self.save_student_changes)
             btn_save.grid(row=5, column=0, columnspan=2, padx=10, pady=10)
         else:
-            messagebox.showwarning("Warning", "An error occurred ")
+            messagebox.showwarning("Warning", "Please select a student to edit")
 
-    def view_student(self):
+    def view_students(self):
         view_window = tk.Toplevel(self)
-        view_window.title("View Student")
+        view_window.title("View Students")
 
-<<<<<<< HEAD
-        title_lable = tk.Label(view_window, text="All Students", font=("ARIAL", 16))
-        title_lable.pack(pady=10)
-=======
-        title_label = tk.Label(view_window, text="All Students", font=("ARIAL", 16))
+        title_label = tk.Label(view_window, text="All Students", font=("Arial", 16))
         title_label.pack(pady=10)
->>>>>>> 5bed0d28272b8076ad726d2aa0abf3e56f15cd33
 
-        student_grid = Treeview(view_window, columns=("meli", "first_name", "last_name", "age", "email"),
+        student_grid = Treeview(view_window, columns=("Meli", "First Name", "Last Name", "Age", "Email"),
                                 show="headings")
 
-        student_grid.heading("meli", text="Meli Code")
-<<<<<<< HEAD
-        student_grid.heading("first_name", text="first_name")
-        student_grid.heading("last_name", text="last_name")
-        student_grid.heading("age", text="age")
-        student_grid.heading("email", text="email")
-=======
-        student_grid.heading("first_name", text="First Name")
-        student_grid.heading("last_name", text="Last Name")
-        student_grid.heading("age", text="Age")
-        student_grid.heading("email", text="Email")
->>>>>>> 5bed0d28272b8076ad726d2aa0abf3e56f15cd33
+        student_grid.heading("Meli", text="Meli Code")
+        student_grid.heading("First Name", text="First Name")
+        student_grid.heading("Last Name", text="Last Name")
+        student_grid.heading("Age", text="Age")
+        student_grid.heading("Email", text="Email")
+
         student_grid['show'] = 'headings'
 
         def on_select(event):
@@ -176,27 +149,40 @@ class StudentManagementApp(tk.Tk):
 
         student_grid.bind("<<TreeviewSelect>>", on_select)
 
-        students = self.database.get_all_student()
+        students = self.database.get_all_students()
 
         for student in students:
             student_grid.insert("", tk.END, values=student)
 
         student_grid.pack(fill=tk.BOTH, expand=True)
 
-    def del_student(self):
+    def delete_student(self):
         if self.selected_student:
-            try:
-                if messagebox.askyesno("Confirm Deletion", "Are you sure you want to delete this this student?"):
-                    meli_code = self.selected_student[0]
-                    self.database.delete_student(meli_code)
+            # Create a confirmation message box
+            confirmation = messagebox.askyesno("Confirmation", "Are you sure you want to delete this student?")
 
-                    self.selected_student = None
-
-
-            except:
-                messagebox.showerror("Error", "An error occurred while deleting student!")
+            # Check user's response
+            if confirmation:
+                meli_code = self.selected_student[0]
+                self.database.delete_student(meli_code)
+                self.view_students()
+                self.clear_entries()
         else:
-            messagebox.showwarning("Warning", "Please select a student to delete!")
+            messagebox.showwarning("Warning", "Please select a student to delete")
+
+    def save_student_changes(self):
+        meli = self.entry_edit_id.get()
+        first_name = self.entry_edit_first_name.get()
+        last_name = self.entry_edit_last_name.get()
+        age = self.entry_edit_age.get()
+        email = self.entry_edit_email.get()
+
+        if meli and first_name and last_name and age and email:
+            person = Person(meli, first_name, last_name, age, email)
+            self.database.update_student(meli, person)
+            messagebox.showinfo("Success", "Student information updated")
+        else:
+            messagebox.showwarning('Error', "Please fill in all the fields")
 
     def clear_entries(self):
         self.entry_id.delete(0, tk.END)
